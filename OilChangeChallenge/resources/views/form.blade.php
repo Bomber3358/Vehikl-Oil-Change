ec752866f658d9a8bf0d2d036698d583c0838557
Bomber3358 <alexdavidson4@gmail.com>
2026-09-13 18:43:08 -0400

    Created Project

---
+++ OilChangeChallenge/resources/views/form.blade.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Oil Change Form</title>
+    </head>
+    <body>
+        <form>
+            <label for="currOdometer">Current odometer value</form><br>
+            <input type = "text" id = "currOdometer", name = "currOdometer"><br>
+            <label for="lastDate">Date of prior change</form><br>
+            <input type = "text" id = "lastDate", name = "lastDate"><br>
+            <label for="prevOdometer">Odometer at last change</form><br>
+            <input type = "text" id = "prevOdometer", name = "prevOdometer"><br>
+        </form>
+    </body>
